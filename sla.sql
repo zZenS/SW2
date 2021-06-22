96e35ff3541118fe6612dcd825d95a614de81882
zZenS <85497989+zZenS@users.noreply.github.com>
2021-06-22 17:56:07 -0300

    Add files via upload

---
+++ sla.sql
@@ -0,0 +1,10 @@
+INSERT INTO orders ( ORD_NUM, ORD_AMOUNT, ADVANCE_AMOUNT, ORD_DATE, CUST_CODE, AGENT_CODE, ORD_DESCRIPTION) VALUES 
+('300100', '1000000.00', '8000.00', '2021-06-06', 'C0110', 'A003', 'SOD')
+
+/* Inseri o code acima 10 vezes pq euu sou desses*/
+
+UPDATE orders SET ORD_DESCRIPTION = 'ATUALIZADO'
+
+DELETE FROM orders 
+
+/* agr o banco morreu/*
---
+++ trabalho.php
@@ -0,0 +1,61 @@
+
+
+
+<?php 
+
+
+$a = 4;
+$b = -4;
+$c = -24;
+$B = -4;
+$q = 4;
+$d = 400;
+$h = 8;
+$k= 24;
+$w = -16;
+
+// Variaveís A,B,C e são os números de delta, e D é o próprio resultado de Delta.
+// Q= -4 da formula de baskahara
+// H= é o resultado do denominador da fração x1 e x2 de 2*a 
+// K= resultado da raiz quadrada de delta, positivio do x1
+// W= resultado da raiz quadrada de delta em sua foram negativa
+
+
+echo $b*$B-$q*$a*$c;
+echo "<br>";
+echo "<br>";
+echo  $k/$h;
+echo "<br>";
+echo "<br>";
+
+echo $w/$h ;
+echo "<br>";
+echo "<br>";
+
+echo "400 é o resultado de delta";
+echo "<br>";
+echo "<br>";
+
+echo "3 é o resultado do x1";
+echo "<br>";
+echo "<br>";
+
+echo "-2 é o resultado de x2";
+echo "<br>";
+echo "<br>";
+echo "A expressão original é 4x2 -4x-24=0";
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
